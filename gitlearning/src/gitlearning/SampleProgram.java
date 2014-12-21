e98cfa58e139bd64f31fddfc1b102f934215a962
vijay <vbestha@apple.com>
2014-12-21 00:22:46 -0800

    added some methods

--- gitlearning/src/gitlearning/SampleProgram.java
+++ gitlearning/src/gitlearning/SampleProgram.java
@@ -2,4 +2,17 @@ package gitlearning;
 
 public class SampleProgram {
 
+	
+	public static void main(String[] args){
+	
+		
+	}
+	
+	public static void add(){
+		
+		System.out.println("iam git");
+		//TODO for loop to be added
+	}
+	
+	
 }
